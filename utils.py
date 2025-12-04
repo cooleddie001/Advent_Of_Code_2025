312c56745b20d642d8edd9ff992e32c96e5e28d8
cooleddie001 <x@cooleddie001.com>
2025-12-04 10:51:43 -0500

    cleanup

--- utils.py
+++ utils.py
@@ -3,5 +3,10 @@ import os
 def get_input(file_path):
     base_name = os.path.basename(file_path)
     file_name, _ = os.path.splitext(base_name)
-    with open(f"Inputs/{file_name}.txt", "r", encoding="utf-8") as f:
-        return f.read()
+    file_name = file_name.split("-")[0] + ".txt"
+    try:
+        with open(f"Inputs/{file_name}", "r", encoding="utf-8") as f:
+            return f.read()
+    except FileNotFoundError:
+        with open(f"../Inputs/{file_name}", "r", encoding="utf-8") as f:
+            return f.read()
